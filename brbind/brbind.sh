64c08ac5f5bd0a4b1eb0e7e4173c1eda628f39f9
Joachim Wiberg <troglobit@gmail.com>
2022-04-07 15:17:51 +0200

    brbind: initial version with dummy interfaces as ports
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ brbind/brbind.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+ip link add p1 type dummy
+ip link add p2 type dummy
+
+ip link add br0 type bridge vlan_filtering 1
+ip link set p1 master br0
+ip link set p2 master br0
+
+# Move port p2 to VLAN 2
+bridge vlan add dev p2 vid 2 pvid untagged
+bridge vlan del dev p2 vid 1
+
+# Bridge tagged in all VLANs
+bridge vlan add dev br0 vid 1 self
+bridge vlan add dev br0 vid 2 self
+
+# Use VLAN interfaces on top of bridge instead
+ip link add link br0 name vlan1 type vlan id 1
+ip link add link br0 name vlan2 type vlan id 2
+
+for iface in p1 p2 br0 vlan1 vlan2; do
+	ip link set $iface up
+done
+
+ip link set vlan1 type vlan bridge_binding on
+ip link set vlan2 type vlan bridge_binding on
+
